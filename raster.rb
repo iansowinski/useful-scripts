f51575fadcb20cde7368f06aec66f180cad9e106
Ian Sowinski <sowinj@gmail.com>
2017-05-14 11:09:53 +0200

    small changes

--- raster.rb
+++ raster.rb
@@ -1,53 +1,62 @@
 require 'rmagick'
 include Magick
+require 'thread'
 
 $start = Time.now
 $this = $start
+$threads = []
 def time
-  puts " : #{(Time.now - $this).round(1)}s"
+  var = " : #{(Time.now - $this).round(1)}s"
   $this = Time.now
-  return nil
+  return var
 end
 
 width = 11811 
 height = 8268
-virtual = Image.new(2, 2)
 
-print ""
+img = Image.read("123.jpg").first()
+puts "loading#{time()}"
+org_width = img.columns
+org_height = img.rows
 
-print "loading"
-img = Image.read("123.jpg").first()#.resize_to_fit!(width, height)
-time()
+puts "#{org_width} x #{org_height}"
 
-print "converting to cmyk"
 img.colorspace = Magick::CMYKColorspace
-time()
+puts "converting to cmyk#{time()}"
 
-print "channel separation"
+
 a = img.separate(AllChannels)
-time()
+puts "channel separation#{time()}"
 
-channels = ["c", "m", "y", "k"]
+channels = ["CYAN", "MAGENTA", "YELLOW", "KEY"]
 degrees = [15, 75, 0, 45]
 
 a.each_with_index do |channel, index|
   next if index == 4
-  puts "#{channels[index]}:"
-  print "  resizing"
-  channel = channel.resize_to_fill(width,height)
-  time()
-  print "  rotating"
-  channel = channel.rotate(degrees[index])
-  time()
-  print "  dither"
-  channel = channel.ordered_dither('h8x8o')
-  time()
-  print "  rotating back"
+  
+  # channel = channel.resize_to_fill(width,height)
+  # puts "#{channels[index]}: resizing#{time()}"
+  
+  print "#{channels[index]}: rotating"
   channel = channel.rotate(-degrees[index])
-  time()
-  print "  writing"
+  puts time()
+  
+  print "#{channels[index]}: dither"
+  channel = channel.ordered_dither('h8x8o')
+  puts time()
+  
+  print "#{channels[index]}: rotating back"
+  channel = channel.rotate(degrees[index])
+  puts time()
+
+  channel = channel.crop(channel.columns/2, channel.rows/2, org_width, org_height)
+  
+  print "#{channels[index]}: writing"
   channel.write("#{channels[index]}.jpg")
-  time()
+  puts time()
 end
 print "Task ran for #{Time.now-$start}"
+
+# Uncomment if on debian
+#`sudo find /tmp/ -name "magick-*" -type f -delete`
 #http://www.imagemagick.org/Usage/quantize/#ordered-dither
